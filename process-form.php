d19d09110f7e3e37caf09be541a90953ec886346
Aishwarya Belekar <abelekar@asu.edu>
2023-12-19 23:43:50 -0700

    Add files via upload

---
+++ process-form.php
@@ -0,0 +1,31 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Retrieve form data
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $message = $_POST["message"];
+
+    // Replace 'your-email@example.com' with your actual email address
+    $to = "abelekar@asu.com";
+    $subject = "New Message from $name";
+
+    // Email headers
+    $headers = "From: $email\r\n";
+    $headers .= "Reply-To: $email\r\n";
+    $headers .= "Content-type: text/plain;charset=utf-8\r\n";
+
+    // Compose and send the email
+    $mailSuccess = mail($to, $subject, $message, $headers);
+
+    // Handle success or failure
+    if ($mailSuccess) {
+        echo "Message sent successfully!";
+    } else {
+        echo "Error sending message. Please try again later.";
+    }
+} else {
+    // Redirect back to the form if accessed directly
+    header("Location: index.html");
+    exit();
+}
+?>
